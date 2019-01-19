6fb8608cf11826c58993813060c49ebcbf1f1379
Michal <misiek-pl@o2.pl>
2019-01-19 11:07:49 +0100

    skonczono task1-drugie rozwiazanie

--- programming1/src/main/java/pl/sda/recap/Task1Demo.java
+++ programming1/src/main/java/pl/sda/recap/Task1Demo.java
@@ -1,15 +1,22 @@
 package pl.sda.recap;
 
 public class Task1Demo {
+    static int metoda(int dlugosc) {
+        int[] tablica = new int[dlugosc];
+        for (int i = 0; i < tablica.length-1; i++) {
+            tablica[i] = i;
+            System.out.println(tablica[i++]);
+        }
+        return dlugosc;
+    }
+
     public static void main(String[] args) {
         //Napisz program, który wypisze co drugi element tablicy przekazanej jako parametr
 
-        int[] tablica =new int[10];
 
-        for (int i = 0; i < tablica.length ; i++) {
-            tablica[i]=i;
-            System.out.println(tablica[i++]);
-        }
+        System.out.println(metoda(100));
+
 
     }
+
 }
