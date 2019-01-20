f5747ec589b8ea44c32aa4aeafec60a67d04e779
Michal <misiek-pl@o2.pl>
2019-01-20 15:48:28 +0100

    koniec zajec

--- programming1/src/main/java/pl/sda/data_structures/SimpleArrayListDemo.java
+++ programming1/src/main/java/pl/sda/data_structures/SimpleArrayListDemo.java
@@ -2,12 +2,13 @@ package pl.sda.data_structures;
 
 public class SimpleArrayListDemo {
     public static void main(String[] args) {
-        SimpleArrayList simpleArrayList=new SimpleArrayList();
+        SimpleArrayList simpleArrayList = new SimpleArrayList();
 
         simpleArrayList.add(2);
         simpleArrayList.add(3);
         simpleArrayList.add(4);
-        System.out.println((simpleArrayList.isEmpty() ? "Empty":"Not empty")+ "\nList size: "+ simpleArrayList.getSize());
+        System.out.println((simpleArrayList.isEmpty() ? "Empty" : "Not empty") +
+                "\nList size: " + simpleArrayList.getSize());
         System.out.println(simpleArrayList.get(2));
 
     }
